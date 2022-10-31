c34624e0366b949fc5d828784a3030a04399c5f1
Amir Rahemeto <amiroimage@gmail.com>
2022-10-31 12:30:24 -0700

    2 and 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -50,9 +50,9 @@ int main(int argc, char *argv[])
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
-			nwr = write(file_to, buf, nchars);
-			if (nwr == -1)
-				error_file(0, -1, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
 	}
 
 	err_close = close(file_from);
