46b7440517fc275521c0be45922ab9dc1f094eb3
Amir Rahemeto <amiroimage@gmail.com>
2022-12-08 11:43:37 -0800

    correction

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -106,13 +106,16 @@ int main(int argc, char **argv)
 	}
 	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
 	/* ----------- f3 ----------- */
-	ch = 1;										vch = 0;
+	ch = 1;
+	vch = 0;
 	while (vch < len)
 	{
-		ch = argv[1][vch] * ch;								vch = vch + 1;
+		ch = argv[1][vch] * ch;
+		vch = vch + 1;
 	}
 	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
-	/* ----------- f4 ----------- */						keygen[3] = ((char *)alph)[f4(argv[1], len)];
+	/* ----------- f4 ----------- */
+	keygen[3] = ((char *)alph)[f4(argv[1], len)];
 	/* ----------- f5 ----------- */
 	keygen[4] = ((char *)alph)[f5(argv[1], len)];
 	/* ----------- f6 ----------- */
